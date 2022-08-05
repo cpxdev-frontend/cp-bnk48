71f3fe0c7202730cb680ec2456d727e4cbbef491
chinnathorn.pro <chinnathorn.pro@cubeofnine.asia>
2022-08-05 10:49:42 +0700

    Event time released

--- src/component/memberdetail.js
+++ src/component/memberdetail.js
@@ -543,13 +543,13 @@ function capitalizeFirstLetter(string) {
                                             <p className='text-muted mt-3' data-aos="zoom-in">{ita.desc}&nbsp;
                                             {ita.timerange[0] > 0 && ita.timerange[1] == 0 && ita.timerange[0] <= moment().unix() && (
                                                 <span className='mt-1 mb-3 badge badge-success'>
-                                                    Current Event
+                                                    Event is starting
                                                 </span>
                                                 )}
                                                 {ita.timerange[0] > 0 && ita.timerange[1] > 0 && ita.timerange[0] < ita.timerange[1] &&
                                                 moment().unix() >= ita.timerange[0] && moment().unix() <= ita.timerange[1] && (
                                                 <span className='mt-1 mb-3 badge badge-success'>
-                                                   Current Event
+                                                     Event is starting
                                                 </span>
                                                 )}
                                                 </p>

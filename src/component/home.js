1609911a9117dc0ce4ad3f5936ec8dc9e21fedaa
Chinnathorn Promnaruritr <56143786+cpx2017@users.noreply.github.com>
2022-09-15 16:22:31 +0700

    Update home.js

--- src/component/home.js
+++ src/component/home.js
@@ -242,9 +242,9 @@ const HomeCom = ({fet, gp, ImgThumb, stream, kamin, setSec}) => {
   )}
   <hr />
   {onMonth ? (
-  <h3 className='mb-5' data-aos="zoom-in-up">BNK48 Members Birthday in this month</h3>
+  <h3 className='mb-5' data-aos="flip-up">BNK48 Members Birthday in this month</h3>
   ) : (
-    <h3 className='mb-5' data-aos="zoom-in-up">BNK48 Members Birthday in today</h3>
+    <h3 className='mb-5' data-aos="flip-up">BNK48 Members Birthday in today</h3>
   )}
   {Loaded1 ? (
       <div className='row ml-3 mr-3 justify-content-center'>
@@ -272,7 +272,7 @@ const HomeCom = ({fet, gp, ImgThumb, stream, kamin, setSec}) => {
     <img src="https://cdn.jsdelivr.net/gh/cpx2017/cpxcdnbucket@main/main/bnk-circular.svg" width="50px" className='text-center' />
   )}
   < hr />
-  <CardHeader title={(<h3 data-aos="zoom-in-down">Sample Members</h3>)} subheader={GenRan != 0 ? ('Generation ' + GenRan) : ''} className='mb-5' />
+  <CardHeader title={(<h3 data-aos="flip-up">Sample Members</h3>)} subheader={GenRan != 0 ? ('Generation ' + GenRan) : ''} className='mb-5' />
   {Loaded2 ? (
       <div className='row ml-3 mr-3 justify-content-center'>
       {samplemem.length > 0 ? samplemem.map((item, i) => (

1020a2d888dd26c5999098bae3fcdb67ad700f66
Chinnathorn Promnaruritr <56143786+cpx2017@users.noreply.github.com>
2022-09-20 16:32:31 +0700

    Update sportday.js

--- src/component/sportday.js
+++ src/component/sportday.js
@@ -24,13 +24,13 @@ const Stream = ({fet, setSec}) => {
         <Card>
             <CardContent className='text-center align-center'>
             <CardHeader title='[Rerun] BNK48 and CGM48 Sport Day 2022 - งานกีฬาสีอนุบาลหนูน้อย 48' subheader='This video streaming may be unusable when you watch from outside of Thailand' />
-            {{window.innerWidth > 800 ? (
+            {window.innerWidth > 800 ? (
                      <div className='container' data-aos="zoom-out-up">
                      <iframe src="https://s.cpxdev.tk/iam48sportday2022" width="100%" height='850px' allowFullScreen />
                      </div>
             ) : (
                 <Button color='primary' variant='contained' onClick={() => window.open('https://s.cpxdev.tk/iam48sportday2022', '_blank')}>Watch it in AIS Play Application</Button>
-            )}}
+            )}
           
             </CardContent>
         </Card>

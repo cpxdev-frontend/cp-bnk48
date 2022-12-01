d39879888f9ee24238587888e435bde47ccb3e84
chinnathorn.pro <chinnathorn.pro@cubeofnine.asia>
2022-12-01 17:17:35 +0700

    Pos

--- src/component/showtime.js
+++ src/component/showtime.js
@@ -48,26 +48,26 @@ const ShowTime = ({fet, setSec}) => {
                             />
                             <Typography variant="h6" className='mt-3'>
                                 {item.title}
+                                {moment.utc() < moment.utc(item.dooropen) ? (
+                                    <span className='badge badge-warning'>
+                                        On scheduled
+                                    </span>
+                                ) : moment.utc(item.dooropen) >= moment.utc() && moment.utc() < moment.utc(item.dateStart) ? (
+                                    <span className='badge badge-info'>
+                                    Door is opening
+                                </span>
+                            ) : moment.utc(item.dateStart) >= moment.utc() && moment.utc() < moment.utc(item.dateEnd) ? (
+                                    <span className='badge badge-success'>
+                                        Currently Showed
+                                    </span>
+                                ): (
+                                        <span className='badge badge-danger'>
+                                            The show is over
+                                        </span>
+                                    )}
                             </Typography>
                     
                             <hr />
-                            {moment.utc() < moment.utc(item.dooropen) ? (
-                                 <span className='badge badge-warning'>
-                                    On scheduled
-                                </span>
-                            ) : moment.utc(item.dooropen) >= moment.utc() && moment.utc() < moment.utc(item.dateStart) ? (
-                                <span className='badge badge-info'>
-                                   Door is opening
-                               </span>
-                           ) : moment.utc(item.dateStart) >= moment.utc() && moment.utc() < moment.utc(item.dateEnd) ? (
-                                <span className='badge badge-success'>
-                                    Currently Showed
-                                </span>
-                            ): (
-                                    <span className='badge badge-danger'>
-                                        The show is over
-                                    </span>
-                                )}
                             <Typography color="textSecondary">
                             Scheduled Showtime: {moment.utc(item.dateStart).local().format('DD MMMM YYYY HH:mm:ss')} - {moment.utc(item.dateEnd).local().format('HH:mm:ss')} (Door open {moment.utc(item.dooropen).local().format('HH:mm:ss')})
                             </Typography>

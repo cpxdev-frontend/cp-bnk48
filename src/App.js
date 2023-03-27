c1c1debbcb8f5b57a8ab4e6fea78a820e0f1df93
chinnathorn.pro <chinnathorn.pro@cubeofnine.asia>
2023-03-27 15:01:01 +0700

    Emergency update

--- src/App.js
+++ src/App.js
@@ -919,7 +919,7 @@ React.useEffect(() => {
   
 
 
-        <footer className={'fixed-bottom text-center text-dark bg-light'} ref={ref}>
+        <footer className={'fixed-bottom text-center text-dark bg-light pt-2'} ref={ref}>
         <h6>Copyright {new Date().getFullYear()}, CPXDevStudio Allright Reserved
         <br /> All BNK48 and CGM48 contents are licensed by Independent Artist Management (iAM). These member images and all events poster is objective for BNK48 supporting only.</h6>
       </footer>

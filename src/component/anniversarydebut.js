892db1aa07285152cf0b346896614d2b83707ffa
chinnathorn.pro <chinnathorn.pro@cubeofnine.asia>
2023-05-26 14:47:24 +0700

    Update p

--- src/component/anniversarydebut.js
+++ src/component/anniversarydebut.js
@@ -93,7 +93,7 @@ const Anni = ({fet, setSec, width}) => {
         {width >1200 && (
           <div class="video-background">
            <Fade in={true} timeout={800}>
-           <img src="https://cdn.statically.io/gl/cpx2017/cpxcdnbucket@main/bnk48/sub13th.jpg" width={width} />
+           <img src="https://cdn.statically.io/gl/cpx2017/cpxcdnbucket@main/bnk48/bnkanni.png" width={width} />
               </Fade>
       </div>
         )}

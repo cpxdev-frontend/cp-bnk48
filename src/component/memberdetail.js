7e3cc34974553151b209c484f79d89cbf2ff36d6
chinnathorn.pro <chinnathorn.pro@cubeofnine.asia>
2023-09-04 10:11:18 +0700

    update 4thalbum

--- src/component/memberdetail.js
+++ src/component/memberdetail.js
@@ -588,28 +588,28 @@ function capitalizeFirstLetter(string) {
                                 <div className='col-md mt-5 mb-5'>
                                     <h4>{item.fullnameEn[0]} {item.fullnameEn[1]} [{item.name}]
                                     </h4>
-                                        {item.sing14 != undefined && item.sing14 == '' && width <= 600 && (
+                                        {/* {item.sing14 != undefined && item.sing14 == '' && width <= 600 && (
                                             <marquee className='cur' onClick={() => session14thSingle(item)}>BNK48 14th Single "Promise - สัญญานะ" is now on music streaming! Please click here to download {item.name}'s solo member image.<br/></marquee>
                                         )}
                                          {item.sing14 != undefined && item.sing14 == '' && width > 600 && (
                                             <p className='cur' onClick={() => session14thSingle(item)}>BNK48 14th Single "Promise - สัญญานะ" is now on music streaming! Please click here to download {item.name}'s solo member image.<br/></p>
-                                        )}
+                                        )} */}
 
 
-                                        {/* {item["4tha"] != undefined && item["4tha"] == '' && width <= 600 && (
+                                        {item["4tha"] != undefined && item["4tha"] == '' && width <= 600 && (
                                             <marquee className='cur' onClick={() => session4thAl(item)}>BNK48 4th Album "Gingham Check" is now on music streaming! Please click here to download {item.name}'s solo member image.<br/></marquee>
                                         )}
                                          {item["4tha"] != undefined && item["4tha"] == '' && width > 600 && (
                                             <p className='cur' onClick={() => session4thAl(item)}>BNK48 4th Album "Gingham Check" is now on music streaming! Please click here to download {item.name}'s solo member image.<br/></p>
-                                        )} */}
+                                        )}
 
 
-                                        {janken != null && width <= 600 && (
+                                        {/* {janken != null && width <= 600 && (
                                             <marquee className='cur' onClick={() => History.push('/janken')}>[BNK48 Janken Tounament 2023] {item.name} BNK48 is one of Senbatsu of BNK48 4th Album "Gingham Check" by Janken Tournament 2023 result by winning {janken.jankenScore} times.<br/></marquee>
                                         )}
                                          {janken != null && width > 600 && (
                                             <p className='cur' onClick={() => History.push('/janken')}>[BNK48 Janken Tounament 2023] {item.name} BNK48 is one of Senbatsu of BNK48 4th Album "Gingham Check" by Janken Tournament 2023 result by winning {janken.jankenScore} times.<br/></p>
-                                        )}
+                                        )} */}
 
 
                                        {loadfollow ? (

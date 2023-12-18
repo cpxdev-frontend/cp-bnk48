a0feef85cf6211b507fc3632af30e80c488135c4
chinnathorn.pro <chinnathorn.pro@cubeofnine.asia>
2023-12-18 10:22:08 +0700

    Quick Update: [2023-12-18 10:22:08.515]

--- src/component/members.js
+++ src/component/members.js
@@ -233,7 +233,7 @@ const Memberlist = ({fet, setSec, width}) => {
               </Grid>
             ) : (
                 <Grid container className='mt-3 justify-content-center'>
-                <Grid item md={3} sm={12} data-aos="fade-in" data-aos-duration="200">
+                <Grid item md={3} xs={12} data-aos="fade-in" data-aos-duration="200">
                 <div className='mb-5 p-1'>
                   <Card>
                   <CardActionArea>
@@ -245,7 +245,7 @@ const Memberlist = ({fet, setSec, width}) => {
                           </Card> 
                       </div>
                 </Grid>
-                <Grid item md={3} sm={12} data-aos="fade-in" data-aos-duration="200">
+                <Grid item md={3} xs={12} data-aos="fade-in" data-aos-duration="200">
                 <div className='mb-5 p-1'>
                   <Card>
                   <CardActionArea>
@@ -257,7 +257,7 @@ const Memberlist = ({fet, setSec, width}) => {
                           </Card> 
                       </div>
                 </Grid>
-                <Grid item md={3} sm={12} data-aos="fade-in" data-aos-duration="200">
+                <Grid item md={3} xs={12} data-aos="fade-in" data-aos-duration="200">
                 <div className='mb-5 p-1'>
                   <Card>
                   <CardActionArea>
@@ -269,7 +269,7 @@ const Memberlist = ({fet, setSec, width}) => {
                           </Card> 
                       </div>
                 </Grid>
-                <Grid item md={3} sm={12} data-aos="fade-in" data-aos-duration="200">
+                <Grid item md={3} xs={12} data-aos="fade-in" data-aos-duration="200">
                 <div className='mb-5 p-1'>
                   <Card>
                   <CardActionArea>

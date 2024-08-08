dfeeca4d8d5d2c5d420bc141666e0794e14f6b16
chinnathorn.pro <chinnathorn.pro@cubeofnine.asia>
2024-08-08 09:42:03 +0700

    Quick Update: [2024-08-08 09:42:03.510]

--- src/component/follow.js
+++ src/component/follow.js
@@ -19,7 +19,7 @@ const Contact = ({fet, setSec, width}) => {
         {width > 700 && (
           <div class="video-background">
            <Fade in={true} timeout={800}>
-           <img src="https://www.bnk48.com/data/Slidebanners/54/img_resize/110714hnqsu7.jpg" width={window.innerWidth} />
+           <img src="https://www.bnk48.com/data/Slidebanners/55/img_resize/015217egklt8.png" width={window.innerWidth} />
               </Fade>
       </div>
         )}

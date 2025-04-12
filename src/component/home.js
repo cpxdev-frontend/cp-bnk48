b6663894f2ca3907a0d1e10bad683edeb5de7773
CPXDev <cpxdev@outlook.com>
2025-04-12 22:21:09 +0700

    Update

--- src/component/home.js
+++ src/component/home.js
@@ -17,7 +17,7 @@ import { Typography, ListItem, Zoom, ListItemText,
 // const defaultTheme = 'https://d3hhrps04devi8.cloudfront.net/bnk48/16thsinggrand.jpg'
 // const defaultvideo = 'https://www.youtube.com/embed/O7mtKDllAg0?autoplay=1&mute=1&controls=0&loop=1&playlist=O7mtKDllAg0'
 const defaultTheme = 'https://image-cdn-ak.spotifycdn.com/image/ab67618600000194a35934b2db09e5b3a73c1dd0'
-const defaultvideo = 'https://www.youtube.com/embed/ymTOSdkUWBs?autoplay=1&mute=1&controls=0&loop=1&playlist=ymTOSdkUWBs'
+const defaultvideo = 'https://www.youtube.com/embed/Pcbp5MYnHJA?autoplay=1&mute=1&controls=0&loop=1&playlist=Pcbp5MYnHJA'
 const HomeCom = ({fet, gp, ImgThumb, stream, kamin, setSec, width}) => {
     const History = useHistory()
     const [Loaded1, setLoaded1] = React.useState(false);
